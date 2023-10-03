909793e34b18c6a80e1338032432c14cf5e7b19e
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-03 16:42:42 -0400

    creating as schema that I'm adding to models

---
+++ models/subscriber.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    subscriberToChannel: {
+                
+    },
+    subscribeDate: {
+
+    }
+})
